6933f94528029b19f1b1e762eb4523736f0f0d85
cetindere <cetinderedc@gmail.com>
2020-03-03 21:23:17 -0500

    updated Login.java

--- src/test/java/com/cybertek/tests/day2_Locators/Login.java
+++ src/test/java/com/cybertek/tests/day2_Locators/Login.java
@@ -14,25 +14,26 @@ public class Login {
         driver.manage().window().maximize();
         /*
         User Story: As a user, I shoul be able to login VyTrack app.
-
-        1. go to VyTrack login page
-        2. write username data: storemanager52
-        3. write password: UserUser123
-        4.click login button
+        test cases 1: login seccessfully
+            1. go to VyTrack login page
+            2. write username data: storemanager52
+            3. write password: UserUser123
+            4.click login button
+        test case2:
          */
         driver.findElement(By.id("prependedInput")).sendKeys("storemanager52");
         driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123");
         driver.findElement(By.id("_submit")).click();
 
         // Verify that you are in the home page
-        String exceptedURL="https://app.vytrack.com/";
-        String actualURL=driver.getCurrentUrl();
-        if (exceptedURL.equals(actualURL)){
+        String exceptedTitle="Dashboard";
+        String actualTitle=driver.getTitle();
+        if (exceptedTitle.equals(actualTitle)){
             System.out.println("PASS");
         }else{
             System.out.println("FAIL");
-            System.out.println("Excepted URL: "+exceptedURL);
-            System.out.println("Actual URL: "+actualURL);
+            System.out.println("Excepted URL: "+exceptedTitle);
+            System.out.println("Actual URL: "+actualTitle);
         }
     }
 }
