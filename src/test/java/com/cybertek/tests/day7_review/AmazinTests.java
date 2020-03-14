97d9e9ea8f6477bc8c7d21a48321606f9ba9efab
cetindere <cetinderedc@gmail.com>
2020-03-14 17:06:10 -0400

    some updated

--- src/test/java/com/cybertek/tests/day7_review/AmazinTests.java
+++ src/test/java/com/cybertek/tests/day7_review/AmazinTests.java
@@ -7,20 +7,74 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+// TODO THIS SPECIFIC AMAZON TEST WILL THROW EXCEPTION
 public class AmazinTests {
     // main method to invoke the tests
     public static void main(String[] args) throws InterruptedException {
         nameMatchTest();
+        secondNameMatchTest();
+
     }
 
     // each test will be a separate method
-    /*
-    * open browser
-    * go to amazon
-    * search for any item
-    * remember the name of the first result
-    * click on the first result
-    * verify that product name is same in the product page
+
+    /**
+     * open browser
+     * go to amazon
+     * search for any item
+     * remember the name of the second result
+     * click on the second result
+     * verify that product name is same in the product page
+     */
+    private static void secondNameMatchTest() throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://amazon.com");
+
+        String searchTerm = "disinfectant wipes";
+        WebElement searchInput = driver.findElement(By.id("twotabsearchtextbox"));
+        // OPTION 1: enter search term and hit ENTER button
+//        searchInput.sendKeys(searchTerm + Keys.ENTER);
+        // OPTION 2: Enter search term and click the search button
+        searchInput.sendKeys(searchTerm);
+        WebElement searchBtn = driver.findElement(By.className("nav-input"));
+        searchBtn.click();
+
+        Thread.sleep(2000);
+        // write xpath that captures second result
+        // CHANGE XPATH
+        WebElement secondResult = driver.findElement(By.xpath("(//span[@class='a-size-base-plus a-color-base a-text-normal'])[2]"));
+        // it is prting the value
+        System.out.println(secondResult.getText());
+        // get the name of the second result
+        String expectedName = secondResult.getText();
+        // click on the second result
+        secondResult.click();
+        // creating a new variable
+        Thread.sleep(2000);
+        WebElement productName = driver.findElement(By.id("productTitle"));
+        String actualName = productName.getText();
+
+        System.out.println(actualName);
+
+        if (expectedName.equals(actualName)) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+            System.out.println("expectedName = " + expectedName);
+            System.out.println("actualName = " + actualName);
+        }
+        driver.quit();
+    }
+
+    // each test will be a separate method
+
+    /**
+     * open browser
+     * go to amazon
+     * search for any item
+     * remember the name of the first result
+     * click on the first result
+     * verify that product name is same in the product page
      */
     private static void nameMatchTest() throws InterruptedException {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -36,25 +90,26 @@ public class AmazinTests {
         searchBtn.click();
 
         Thread.sleep(2000);
-
         // write xpath that captures first result
         WebElement firstResult = driver.findElement(By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']"));
+        // it is prting the value
+        System.out.println(firstResult.getText());
+        // get the name of the first result
         String expectedName = firstResult.getText();
-        System.out.println(expectedName);
+        // click on the first result
         firstResult.click();
-
-        Thread.sleep(2000);
-
+        // creating a new variable
         WebElement productName = driver.findElement(By.id("productTitle"));
         String actualName = productName.getText();
+
         System.out.println(actualName);
 
-        if (expectedName.equalsIgnoreCase(actualName)){
+        if (expectedName.equals(actualName)) {
             System.out.println("PASS");
-        }else{
+        } else {
             System.out.println("FAIL");
-            System.out.println("Expected Name: "+expectedName);
-            System.out.println("Actual Name: "+actualName);
+            System.out.println("expectedName = " + expectedName);
+            System.out.println("actualName = " + actualName);
         }
         driver.quit();
     }
