7d22ffc13c5b0460ebb7bc586aa9de53f36ffaba
cetindere <cetinderedc@gmail.com>
2020-03-23 17:54:23 -0400

    updated

--- src/test/java/com/cybertek/tests/day10_webelement/OrderOfTests.java
+++ src/test/java/com/cybertek/tests/day10_webelement/OrderOfTests.java
@@ -5,24 +5,25 @@ import org.testng.annotations.Test;
 
 public class OrderOfTests {
 
-    String title;
+        String title;
 
-    @Test(priority = 0)
-    public void  login(){
-        System.out.println("opening the browser");
-        System.out.println("capturing title");
-        title = "cbt";
-    }
+        @Test(priority = 0)
+        public void login() {
+            System.out.println("opening the browser");
+            System.out.println("capturing title");
+            title = "cbt";
+            // i will make this test fail.  Assert.fail() --> is a ready method that makes test fail
+            Assert.fail();
+        }
 
-    @Test(priority = 5)
-    public void assertTitle(){
-        System.out.println("verifying the title");
-        Assert.assertEquals(title, "cbt");
+        @Test(priority = 1, dependsOnMethods = "login")
+        public void assertTitle() {
+            System.out.println("verifying the title");
+            Assert.assertEquals(title, "cbt");
+        }
 
-    }
-
-    @Test(priority = 35)
-    public void verifyOtherThings(){
-        System.out.println("verify sth else");
-    }
+        @Test(priority = 55)
+        public void verifyOtherThings() {
+            System.out.println("verify sth else");
+        }
 }
