4ab280fd5a3b8e6d01c3d2e1a8d2cd8fe3dd5c50
cetindere <cetinderedc@gmail.com>
2020-03-28 12:57:17 -0400

    added day13 packege

---
+++ src/test/java/com/cybertek/tests/day13_waits_and_synchronization/ExplicitWaitExamples.java
@@ -0,0 +1,40 @@
+package com.cybertek.tests.day13_waits_and_synchronization;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExplicitWaitExamples {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void test1TitleTest(){
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+
+        driver.get("https://store.steampowered.com/");
+        //wait for the title of the second page
+        //create web object
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        //start waiting for title contain text
+        wait.until(ExpectedConditions.titleContains("Steam"));
+
+        System.out.println(driver.getTitle());
+
+    }
+}
